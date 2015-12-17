1836afd6084159d10878442594423352e84a3749
Robb Hamilton <rhamilto@redhat.com>
2015-12-17 15:58:19 -0500

    Removing unnecessary role attribute from nav

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
   </head>
   <body <?php body_class(); ?>>
     <header role="banner">
-      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
