8533117e175f59b49f090b6c474b5606e5e2b30b
Robb Hamilton <rhamilto@redhat.com>
2015-12-17 15:49:11 -0500

    Custom login page bug fix

--- functions.php
+++ functions.php
@@ -859,7 +859,7 @@ function bootstrapwp_breadcrumbs()
 
 // Add stylesheet to login
 function my_login_stylesheet() { ?>
-    <link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_bloginfo( 'stylesheet_directory' ) . '/library/css/patternfly-site.min.css'; ?>" type="text/css" media="all" />
+    <link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_bloginfo( 'stylesheet_directory' ) . '/library/css/patternfly-blog.min.css'; ?>" type="text/css" media="all" />
 <?php }
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
 
